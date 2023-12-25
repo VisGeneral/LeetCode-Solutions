1684255711cbac98347b998bc6a8d1b8240606ad
Kerry Z <kerryzheng2009@gmail.com>
2023-12-25 15:18:22 -0500

    Time: 80 ms (75.71%) | Memory: 13.5 MB (81.11%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=[]
+        i=0
+        while 0 <= i < len(nums):
+            ans.append(nums[nums[i]])
+            i+=1
+        return ans
