ab7e48baed511c23f2ec570962837ed7dd637d51
Kerry Z <kerryzheng2009@gmail.com>
2023-12-25 15:36:52 -0500

    Time: 8 ms (92.34%) | Memory: 13.3 MB (47.01%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -4,4 +4,7 @@ class Solution(object):
         :type celsius: float
         :rtype: List[float]
         """
-        return [celsius + 273.15, celsius * 1.80 + 32.00]
+        ans=[]
+        ans.append(celsius + 273.15)
+        ans.append(celsius * 1.80 + 32.00)
+        return ans
